fef6e1d153cddc4d4d0adcfdd56bcd021e0aa48f
Diego Perez <Diego.fernando.perez.luna@gmail.com>
2025-04-06 21:07:40 -0400

    Fix: urls to cache fixed

--- 02-pomodoro-tracker/service-worker.js
+++ 02-pomodoro-tracker/service-worker.js
@@ -1,14 +1,26 @@
-const CACHE_NAME = 'pomodoro-cache-v1';
-const urlsToCache = ['/', '/index.html', '/styles.css', '/main.js'];
+const CACHE_NAME = "pomodoro-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/main.js",
+  "/src/components/timer-component/timer.js",
+  "/src/components/timer-component/timer.css",
+  "/src/components/switch-component/theme-switch.js",
+  "/src/components/button-component/button.js",
+  "/src/components/notification/notification.js",
+];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
   );
 });
